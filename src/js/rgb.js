fd5a551a406b379ed48cd1d8f45a445f93dbc48d
dev <dev@admins-MacBook-Pro.local>
2021-07-02 16:58:17 +0200

    changing innerHTML to textContent (security issues) and changing function name to a number to a clearer name getRandoNumber

--- src/js/rgb.js
+++ src/js/rgb.js
@@ -1,17 +1,17 @@
 document.querySelector('#btn_change_color').addEventListener('click', () => {
   
-   function number (){
+   function getRandomNumber (){
         return Math.floor(Math.random() * 256);
     }
 
-    const r = number();
-    const g = number();
-    const b = number();
+    const r = getRandomNumber();
+    const g = getRandomNumber();
+    const b = getRandomNumber();
 
     // The const site is necessary for the opacity slider
     const site = document.querySelector('main');
 
     site.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    document.querySelector('.rgb').innerHTML = `${r},${g},${b}`;
+    document.querySelector('.rgb').textContent = `${r},${g},${b}`;
 });
