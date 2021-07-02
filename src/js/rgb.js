e5e5c5bb1e4bada80af88ef764d1e004bba4751f
dev <dev@admins-MacBook-Pro.local>
2021-07-02 02:33:05 +0200

    new smaller code approach

--- src/js/rgb.js
+++ src/js/rgb.js
@@ -1,18 +1,14 @@
 document.querySelector('#btn_change_color').addEventListener('click', () => {
-    const Number = function() {
-        this.color = function () {
-            return (Math.floor(Math.random() * 256))  
-        }
+  
+   function number (){
+        return Math.floor(Math.random() * 256);
     }
 
-    let randomNumber = new Number();
+    const r = number();
+    const g = number();
+    const b = number();
 
-    const r = randomNumber.color();
-    const g = randomNumber.color();
-    const b = randomNumber.color();
-
-    let site = document.querySelector('.site');
-    site.style.backgroundColor = `rgb(${r},${g},${b})`;
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
 
     document.querySelector('.rgb').innerHTML = `${r},${g},${b}`;
 });
