c7abee3af09c8a87ef36f6723be9d20db7c4147a
dev <dev@admins-MacBook-Pro.local>
2021-07-02 04:11:46 +0200

    formatting the document

--- src/js/opacity.js
+++ src/js/opacity.js
@@ -1,19 +1,18 @@
-const slider = document.querySelector("#myOpacity");
-const output = document.querySelector("#opacity");
+const slider = document.querySelector('.slider');
+const output = document.querySelector('#opacity');
 output.innerHTML = 1; // Display the default slider value
 
-const site = document.querySelector('.site');
+const site = document.querySelector('main');
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-  site.style.opacity = this.value;
-  
-  if(slider.value < 10){
-    site.style.opacity = `0.${slider.value}`;
-    output.innerHTML = `0.${slider.value}`;
-} else {
-    output.innerHTML = 1;
-}
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    site.style.opacity = this.value;
 
-}
+    if (slider.value < 10) {
+        site.style.opacity = `0.${slider.value}`;
+        output.innerHTML = `0.${slider.value}`;
+    } else {
+        output.innerHTML = 1;
+    }
+};
