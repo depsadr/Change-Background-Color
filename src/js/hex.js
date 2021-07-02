398c4ff1ab89f8a4ec32dec8c81171be6c350834
dev <dev@admins-MacBook-Pro.local>
2021-07-02 17:03:22 +0200

    fixing security issues by using textContent instead of innerHTML - creating a function for geting random number (not in for loop anymore) - changing the variable name to a clearer name(hexColor instead of hexCol

--- src/js/hex.js
+++ src/js/hex.js
@@ -1,19 +1,20 @@
+// create array of Hex values
 let hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 document.querySelector('#btn_change_color').addEventListener('click', () => {
-    let hexCol = '#';
+    let hexColor = '#';
 
-    for(let i = 0; i < 6; i++){
-        // for(let hexNumber of hexNumbers){
-            let random = Math.floor(Math.random() * hexNumbers.length);
-            // console.log(random);
-            hexCol += hexNumbers[random];
-            // console.log(hexCol);
-        // }
-
+    function getRandomNumber() {
+        return Math.floor(Math.random() * hexNumbers.length);
     }
-    let site = document.querySelector('.site');
-    site.style.backgroundColor = hexCol;
-    document.querySelector('.hex').innerHTML = hexCol;
-    // console.log(hexCol);
+
+    // loop throgh hexNumbers array
+    for (let i = 0; i < 6; i++) {
+        hexColor += hexNumbers[getRandomNumber()];
+    }
+
+    // output random * 6 = Hex color value
+    let site = document.querySelector('main');
+    site.style.backgroundColor = hexColor;
+    document.querySelector('.hex').textContent = hexColor;
 });
